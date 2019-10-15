d30f64bf1391b2954fee1149c1e205884db73df6
srinivasu.kakumanu <ksrinivasabb@gmail.com>
2019-10-15 21:32:37 +0530

    Modified the header display test

--- Automation-App/Automation_TC/wsgi.py
+++ Automation-App/Automation_TC/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RealState project.
+WSGI config for Automation_TC project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RealState.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Automation_TC.settings')
 
 application = get_wsgi_application()
